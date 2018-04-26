a396caec4402f046dbb3ff2a641a1bf81c813634
Watted <wattk308@gmail.com>
2018-04-26 15:29:02 +0300

    created a Stack

---
+++ Data structures/Stack.js
@@ -0,0 +1,41 @@
+let curr = null;
+
+function push(data) {
+    curr = new Node(data, curr);
+}
+
+function pop() {
+    curr = curr ? curr.prev : null;
+}
+
+function print(){
+    let tmp = curr;
+    while(tmp){
+        console.log(tmp.data);
+        tmp = tmp.prev;
+    }
+}
+
+function Node(data, prev) {
+    this.data = data;
+    this.prev = prev;
+}
+
+(function () {
+    push('123');
+    push('333');
+    push('aaa')
+    print();
+    console.log('====')
+    pop();
+    print();
+    console.log('====')
+    pop();
+    print();
+    console.log('====')
+    pop();
+    print();
+    console.log('====')
+    pop();
+    print();
+})();
