87adc8100c491987da6c24f9c4aa77b0cc7e70e5
Watted <wattk308@gmail.com>
2018-05-02 16:23:25 +0300

    first commit

--- BlackJack/Hand.js
+++ BlackJack/Hand.js
@@ -1,19 +1,30 @@
-class Hand {
-    constructor(){
-        this.hand = new Deck();
-        this.player1 = new Players('Mohammed');
-        this.player2 = new Players('Ofer');
-    }
-    setCard(){
-        this.hand.createDeck();
-        for(var i =0;i< 2;i++){
-            this.player1.setCard(this.hand.giveCard());
-            this.player2.setCard(this.hand.giveCard());
-        }
-    }
-    hit(name){
 
-    }
-
-
+var hand = new Deck();
+var player1 = new Players('Mohammed');
+var player2 = new Players('Ofer');
+run();
+var choice
+function run() {
 }
+
+function setCard() {
+    this.hand.createDeck();
+    for (var i = 0; i < 2; i++) {
+        this.player1.setCard(this.hand.giveCard());
+        this.player2.setCard(this.hand.giveCard());
+    }
+}
+
+function hit(name) {
+    if (name === 'Mohammed') {
+        this.player1.setCard(this.hand.giveCard());
+    } else {
+        this.player2.setCard(this.hand.giveCard());
+    }
+}
+
+function stand(name) {
+    return name === 'Mohammed' ? this.player1.getScoure() : this.player2.getScoure();
+}
+
+
