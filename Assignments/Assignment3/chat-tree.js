35301f78bdc3988c61046fb497a82779495595c5
Watted <wattk308@gmail.com>
2018-05-16 15:44:18 +0300

    solutions

--- Assignments/Assignment3/chat-tree.js
+++ Assignments/Assignment3/chat-tree.js
@@ -1,13 +1,35 @@
 function ChatTree(element) {
+
     function load(items) {
-        for (let item of items){
-            let x = document.createElement("li").value = item["name"];
-            element.append(x);
-        }
+
+        clear();
+        loadItems(items,0);
     }
 
+    function loadItems(items,numberOfSpaces) {
+        for (let item of items){
+
+            let li = $('<li>');
+            li.addClass(item.type);
+            li.append('&nbsp;'.repeat(numberOfSpaces) + item.name);
+            li.appendTo(element);
+            li.click(() => {
+                console.log("heehe");
+            });
+
+            li.dblclick(() => {
+                console.log("asdad");
+            });
+            if (item.items){
+                loadItems(item.items,numberOfSpaces+3);
+            }
+
+        }
+
+    }
     function clear() {
-        element = ''
+        element.innerText = "";
+
     }
 
     return {
